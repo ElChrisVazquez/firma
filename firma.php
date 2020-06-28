da2661874a4eb390f9590fb0d1e22b8fe7019967
ElChrisVazquez <TheChrisVazquez@gmail.com>
2020-06-28 00:08:50 -0500

    Creación del cuadro para firmar y envio de la firma a otro documento

---
+++ firma.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+echo '<pre>' . var_dump($_POST) . '</pre>';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Firma</title>
+</head>
+
+<script type="text/javascript">
+    var canvas, ctx, flag = false,
+        prevX = 0,
+        currX = 0,
+        prevY = 0,
+        currY = 0,
+        dot_flag = false;
+
+    var x = "black",
+        y = 2;
+
+    function init() {
+        canvas = document.getElementById('can');
+        ctx = canvas.getContext("2d");
+        w = canvas.width;
+        h = canvas.height;
+
+        canvas.addEventListener("mousemove", function(e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function(e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function(e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function(e) {
+            findxy('out', e)
+        }, false);
+    }
+
+    function draw() {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.strokeStyle = x;
+        ctx.lineWidth = y;
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    function erase() {
+        var m = confirm("Want to clear");
+        if (m) {
+            ctx.clearRect(0, 0, w, h);
+            document.getElementById("canvasimg").style.display = "none";
+        }
+    }
+
+    function save() {
+        document.getElementById("canvasimg").style.border = "2px solid";
+        var dataURL = canvas.toDataURL();
+        document.getElementById("canvasimg").src = dataURL;
+        document.getElementById("lafirma").value = dataURL;
+    }
+
+    function findxy(res, e) {
+        if (res == 'down') {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+
+            flag = true;
+            dot_flag = true;
+            if (dot_flag) {
+                ctx.beginPath();
+                ctx.fillStyle = x;
+                ctx.fillRect(currX, currY, 2, 2);
+                ctx.closePath();
+                dot_flag = false;
+            }
+        }
+        if (res == 'up' || res == "out") {
+            flag = false;
+        }
+        if (res == 'move') {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+                draw();
+            }
+        }
+    }
+</script>
+
+<body onload="init()">
+    <form action="pdf.php" target="_blank" method="POST">
+        <canvas id="can" width="300" height="100" style="border:2px solid;"></canvas>
+        <br>
+        <br>
+        <input type="submit" value="Firmar" id="btn" size="30" onclick="save()">
+        <br>
+        <br>
+        <input type="button" value="Boirrar" id="clr" size="23" onclick="erase()">
+        <br>
+        <br>
+        <!-- <img id="canvasimg" name="canvas"> -->
+        <input id="lafirma" type="hidden" name="lafirma">
+    </form>
+
+</body>
+
+</html>
---
+++ pdf.php
@@ -0,0 +1,6 @@
+<?php
+echo '<pre>' . var_dump($_POST) . '</pre>';
+
+
+echo  '<img src="'.$_POST["lafirma"].'">';
+?>
